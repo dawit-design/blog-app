9378a8bb1c2d5191de0f30a7b0607e46d5c66d9c
dawit-design <dawitawegchow@gmail.com>
2021-12-02 16:53:29 -0500

    update blog list with useReducer instead of useSate

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,33 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 
 const BlogContext = React.createContext();
+// state can be anything like blogPost or any value but by convention we usually name it state
+const blogReducer = (state,action) => {
+    switch(action.type) {
+        case 'add_blogPost':
+            return [...state, {title: `Blog Post #${state.length + 1}`}]
+        default:
+            return state;    
+    }
+}
 
 export const BlogProvider = ({children} = {}) =>{
-    const [blogPosts, setBlogPosts] = useState([]);
-
-    const addBlogPost = (blogPost) => {
-        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
+    const [blogPosts, dispatch] = useReducer(blogReducer,[]);
+    
+    const addBlogPost = () => {
+        dispatch({type: 'add_blogPost'})
     }
-    // const blogPosts = [{title: 'Blog post #1'}, {title: 'Blog post #2'}, {title: 'Blog post #3'}]
-
     return <BlogContext.Provider value={{data : blogPosts, addBlogPost}}>
         {children}
     </BlogContext.Provider>
 }
 
+
 export default BlogContext;
+
+
+
+    // const addBlogPost = (blogPost) => {
+    //     setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
+    // }
+    // const blogPosts = [{title: 'Blog post #1'}, {title: 'Blog post #2'}, {title: 'Blog post #3'}]
