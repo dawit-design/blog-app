e7c7c32775c6805128a8e776e9829e044aadc9a3
dawit-design <dawitawegchow@gmail.com>
2021-12-02 15:51:40 -0500

    added a placeholder blogpost and rendered

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,9 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({children} = {}) =>{
-    return <BlogContext.Provider value={5}>
+    const blogPosts = [{title: 'Blog post #1'}, {title: 'Blog post #2'}, {title: 'Blog post #3'}]
+
+    return <BlogContext.Provider value={blogPosts}>
         {children}
     </BlogContext.Provider>
 }
