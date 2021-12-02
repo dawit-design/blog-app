2e9677ddcd7953b7e1515154047e8dece222d203
dawit-design <dawitawegchow@gmail.com>
2021-12-02 16:03:55 -0500

    added a state in context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({children} = {}) =>{
-    const blogPosts = [{title: 'Blog post #1'}, {title: 'Blog post #2'}, {title: 'Blog post #3'}]
+    const [blogPosts, setBlogPosts] = useState([]);
 
-    return <BlogContext.Provider value={blogPosts}>
+    const addBlogPost = (blogPost) => {
+        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
+    }
+    // const blogPosts = [{title: 'Blog post #1'}, {title: 'Blog post #2'}, {title: 'Blog post #3'}]
+
+    return <BlogContext.Provider value={{data : blogPosts, addBlogPost}}>
         {children}
     </BlogContext.Provider>
 }
