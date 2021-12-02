1705c85d9b03db9860b22ccea803a5ea293ec9ca
dawit-design <dawitawegchow@gmail.com>
2021-12-02 17:40:47 -0500

    refactor and automate context creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,33 +1,23 @@
-import React, {useReducer} from 'react';
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext();
 // state can be anything like blogPost or any value but by convention we usually name it state
-const blogReducer = (state,action) => {
-    switch(action.type) {
-        case 'add_blogPost':
-            return [...state, {title: `Blog Post #${state.length + 1}`}]
-        default:
-            return state;    
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogPost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = dispatch => {
+    return () => {
+        dispatch({ type: "add_blogPost" });
     }
-}
+};
 
-export const BlogProvider = ({children} = {}) =>{
-    const [blogPosts, dispatch] = useReducer(blogReducer,[]);
-    
-    const addBlogPost = () => {
-        dispatch({type: 'add_blogPost'})
-    }
-    return <BlogContext.Provider value={{data : blogPosts, addBlogPost}}>
-        {children}
-    </BlogContext.Provider>
-}
-
-
-export default BlogContext;
-
-
-
-    // const addBlogPost = (blogPost) => {
-    //     setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
-    // }
-    // const blogPosts = [{title: 'Blog post #1'}, {title: 'Blog post #2'}, {title: 'Blog post #3'}]
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
