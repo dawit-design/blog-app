6ebb4d70ed57141c95ed10e49230365b0558d8e5
dawit-design <dawitawegchow@gmail.com>
2021-12-08 10:25:38 -0500

    delete functionality added

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,21 +3,34 @@ import createDataContext from "./createDataContext";
 // state can be anything like blogPost or any value but by convention we usually name it state
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'delete_blogPost':
+      return state.filter((blogPost) => blogPost.id !== action.payload)
     case "add_blogPost":
-      return [...state, {id: Math.floor(Math.random() * 99999) ,title: `Blog Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ];
     default:
       return state;
   }
 };
 
-const addBlogPost = dispatch => {
-    return () => {
-        dispatch({ type: "add_blogPost" });
-    }
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: "add_blogPost" });
+  };
+};
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_blogPost", payload: id });
+  };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,44 +1,48 @@
-import React, {useContext} from "react";
-import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from "react-native";
-import {Context} from '../context/BlogContext';
-import { Feather } from '@expo/vector-icons';
+import React, { useContext } from "react";
+import {View,Text,StyleSheet,FlatList,Button,TouchableOpacity,} from "react-native";
+import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
-    const {state, addBlogPost} = useContext(Context)
-    return (
-        <View >
-            <Button title="Add Post" onPress={addBlogPost}/>
-            <FlatList 
-                data={state}
-                keyExtractor={(blogPost) => blogPost.title}
-                renderItem={({item}) =>{
-                    return <View style={styles.row}>
-                        <Text style={styles.title}>{item.title} - {item.id}</Text>
-                        <TouchableOpacity onPress={() => console.log(item.id)}>
-                         <Feather style={styles.icon} name="trash" color="black" />
-                        </TouchableOpacity>
-                        </View>
-                }}
-            />
-        </View>
-    )
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+  return (
+    <View>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)} >
+                <Feather style={styles.icon} name="trash"/>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 15,
-        borderTopWidth: 1,
-        borderColor: 'gray',
-        paddingHorizontal: 10
-    },
-    title: {
-        fontSize: 18,
-    },
-    icon: {
-        fontSize: 24
-    }
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: "gray",
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
 });
 
 export default IndexScreen;
